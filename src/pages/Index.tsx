a5597986e75624c7b6bd56b890b50e4bfd8136e4
n <n>
2025-04-27 00:47:28 +0300

    Add Community button with Telegram link

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -37,6 +37,14 @@ const Index = () => {
                 <div className="flex gap-4 justify-center">
                   <Button size="lg" className="bg-white/90 text-black hover:bg-white">Buy Token</Button>
                   <Button size="lg" variant="outline" className="border-white/90 text-slate-50 bg-[#000a0e]/0">Get TIT$ NOW</Button>
+                  <Button 
+                    size="lg" 
+                    variant="outline" 
+                    className="border-white/90 text-slate-50 bg-[#000a0e]/0"
+                    onClick={() => window.open('https://t.me/boobstitsclub', '_blank')}
+                  >
+                    Community
+                  </Button>
                 </div>
               </div>
             </div>
